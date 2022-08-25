f2c315288d45632550044d80a688b445cd08f2f4
eramirez05 <112093739+eramirez05@users.noreply.github.com>
2022-08-25 09:50:55 -0500

    PRIMERA SUBIDA DE PROGRAMAS DE PYTHON

---
+++ CURSO/HolaMundo.py
@@ -0,0 +1,11 @@
+#EJEMPLO 1.SALUDO HOLA MUNDO
+#COMENTARIOS:Se usan para documentar o describir cada parte del código
+#Los comentarios los omite o no los toma en cuenta pyuthon
+
+#Mostrar o imprimir pantalla
+print("Hola Mundo")
+print()
+print("Esmeralda")
+print(5*8)
+
+print("La suma de 5 + 5 =",10);
